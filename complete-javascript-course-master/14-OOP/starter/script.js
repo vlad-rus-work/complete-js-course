e6119a9a08c0a6a43056a0441da9871208ac5a8e
iamvladrus <vlad.rus.work@gmail.com>
2024-11-14 14:57:41 +0200

    updated code

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -1,1 +1,114 @@
-'use strict';
+"use strict";
+/*
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never do this
+  // this.calcAge = function () {
+  //   console.log(2037 - this.birthYear);
+  // };
+};
+
+const vlad = new Person("Vlad", 2002);
+console.log(vlad);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to a prototype
+// 4. function automatically returns {}
+
+// Prototypes
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+vlad.calcAge();
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 2, 4];
+console.log(arr.__proto__);
+*/
+
+/*
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} accelerates! (Current speed ${this.speed}km/h)`);
+};
+
+Car.prototype.break = function () {
+  this.speed -= 5;
+  console.log(`${this.make} breaks! (Current speed ${this.speed}km/h)`);
+};
+
+const BMW = new Car("BMW", 120);
+const mercedes = new Car("Mercedes", 95);
+
+BMW.accelerate();
+BMW.accelerate();
+BMW.accelerate();
+BMW.break();
+
+mercedes.break();
+mercedes.accelerate();
+mercedes.accelerate();
+mercedes.break();
+*/
+
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  // Methods will be added to the .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  // Set a property that already exists
+  set fullName(name) {
+    if (name.includes(" ")) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+}
+
+const jessica = new PersonCl("Jessica Davis", 1996);
+console.log(jessica);
+jessica.calcAge();
+console.log(jessica.age);
+
+const walter = new PersonCl("Walter White", 1965);
+
+const account = {
+  owner: "Jonas",
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+account.latest = 50;
